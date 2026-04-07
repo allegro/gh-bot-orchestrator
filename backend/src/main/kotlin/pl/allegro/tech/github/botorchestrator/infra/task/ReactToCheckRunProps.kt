1bb943387027d13771f89d6c6b1a62bb2672b674
m1chu <m1chu@m1chu.eu>
2026-04-07 22:07:54 +0200

    Fix: rethrow exceptions in task handlers to enable db-scheduler retry.

--- backend/src/main/kotlin/pl/allegro/tech/github/botorchestrator/infra/task/ReactToCheckRunProps.kt
+++ backend/src/main/kotlin/pl/allegro/tech/github/botorchestrator/infra/task/ReactToCheckRunProps.kt
@@ -3,8 +3,8 @@ package pl.allegro.tech.github.botorchestrator.infra.task
 import org.springframework.boot.context.properties.ConfigurationProperties
 import java.time.Duration as JavaDuration
 
-@ConfigurationProperties(prefix = "app.tasks.handle-pull-request-change")
-data class ReactToPullRequestChangeProps(
+@ConfigurationProperties(prefix = "app.tasks.handle-check-run")
+data class ReactToCheckRunProps(
     val sleepDuration: JavaDuration,
     val noSlotsSleepDuration: JavaDuration
 )
